31e7bee708aa0c3916b78229907fba435ebe9089
dengpeng.zuo <cqdzuodp@163.com>
2017-12-07 15:21:14 +0800

    加入idle控制

--- src/main/java/com/ezweb/engine/NettyDecoder.java
+++ src/main/java/com/ezweb/engine/NettyDecoder.java
@@ -20,62 +20,57 @@ public class NettyDecoder extends LengthFieldBasedFrameDecoder {
 	@Override
 	protected Object decode(ChannelHandlerContext ctx, ByteBuf in) throws Exception {
 		ByteBuf frame = (ByteBuf) super.decode(ctx, in);
-		if (null == frame) {
-			return null;
-		}
-		// 读取头信息.
-		int magic = frame.readUnsignedShort();
-		if (magic != CustTMessage.MAGIC) {
-			frame.discardReadBytes();
-			frame.release();
-			throw new Exception("协议Magic无效!");
-		}
-		byte ver_type = frame.readByte();
+		if (null == frame) return null;
 
-		byte ver = (byte) ((ver_type & 0b11110000) >>> 4);
-		byte type = (byte) (ver_type & 0b00001111);
-
-		byte protoType = frame.readByte();
-
-		if (ver == CustTVersion.VERSION_1) {
-			int seqId = frame.readInt();
-			int len = frame.readInt();
-			if (len > CustTMessage.BODY_MAX_LEN) {
+		try {
+			// 读取头信息.
+			int magic = frame.readUnsignedShort();
+			if (magic != CustTMessage.MAGIC) {
 				frame.discardReadBytes();
-				frame.release();
-				throw new Exception("body.len 无效!");
+				throw new Exception("协议Magic无效!");
 			}
-			// 读取crc32.
-			long crc32 = frame.readUnsignedInt();
-			// 读取余下的body.
-			ByteBuffer body = ByteBuffer.allocate(len);
-			frame.readBytes(body);
-			body.flip();      // [0, position]
+			byte ver_type = frame.readByte();
 
-			long crc32_v = NettyEncoder.calcCrc32(ver, type, protoType, seqId, len, body);
-			if (crc32 != crc32_v) {
-				frame.discardReadBytes();
-				frame.release();
-				throw new Exception("crc32 失败");
-			}
+			byte ver = (byte) ((ver_type & 0b11110000) >>> 4);
+			byte type = (byte) (ver_type & 0b00001111);
 
-			CustTMessage msg = new CustTMessage();
-			msg.setVersion(ver);
-			msg.setType(type);
-			msg.setProtoType(protoType);
-			msg.setSeqId(seqId);
-			msg.setLen(len);
-			msg.setBody(body);
+			byte protoType = frame.readByte();
 
-			try {
+			if (ver == CustTVersion.VERSION_1) {
+				int seqId = frame.readInt();
+				int len = frame.readInt();
+				if (len > CustTMessage.BODY_MAX_LEN) {
+					frame.discardReadBytes();
+					throw new Exception("body.len 无效!");
+				}
+				// 读取crc32.
+				long crc32 = frame.readUnsignedInt();
+				// 读取余下的body.
+				ByteBuffer body = ByteBuffer.allocate(len);
+				frame.readBytes(body);
+				body.flip();      // [0, position]
+
+				long crc32_v = NettyEncoder.calcCrc32(ver, type, protoType, seqId, len, body);
+				if (crc32 != crc32_v) {
+					frame.discardReadBytes();
+					throw new Exception("crc32 失败");
+				}
+
+				CustTMessage msg = new CustTMessage();
+				msg.setVersion(ver);
+				msg.setType(type);
+				msg.setProtoType(protoType);
+				msg.setSeqId(seqId);
+				msg.setLen(len);
+				msg.setBody(body);
+
 				return msg;
-			} finally {
-				frame.release(); // 放掉缓存.
 			}
-		} else {
+			// 不支持 version
 			frame.discardReadBytes();
-			ReferenceCountUtil.release(frame);
 			throw new Exception("协议Version无效!");
+		} finally {
+			ReferenceCountUtil.release(frame);
 		}
 	}
 }
--- src/main/java/com/ezweb/engine/server/NettyServer.java
+++ src/main/java/com/ezweb/engine/server/NettyServer.java
@@ -2,21 +2,24 @@ package com.ezweb.engine.server;
 
 import com.ezweb.engine.NettyDecoder;
 import com.ezweb.engine.NettyEncoder;
+import com.ezweb.engine.util.RemotingUtil;
 import io.netty.bootstrap.ServerBootstrap;
 import io.netty.buffer.PooledByteBufAllocator;
-import io.netty.channel.ChannelFuture;
-import io.netty.channel.ChannelInitializer;
-import io.netty.channel.ChannelOption;
-import io.netty.channel.EventLoopGroup;
+import io.netty.channel.*;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
 import io.netty.handler.logging.LogLevel;
 import io.netty.handler.logging.LoggingHandler;
+import io.netty.handler.timeout.IdleState;
+import io.netty.handler.timeout.IdleStateEvent;
+import io.netty.handler.timeout.IdleStateHandler;
 import io.netty.util.concurrent.DefaultEventExecutorGroup;
 import io.netty.util.concurrent.DefaultThreadFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author : zuodp
@@ -71,15 +74,18 @@ public class NettyServer {
 
 	private ServerBootstrap configServer() {
 		bossGroup = new NioEventLoopGroup(1, new DefaultThreadFactory("NettyBossGroup"));
-		workerGroup = new NioEventLoopGroup(3, new DefaultThreadFactory("NettyWorkerGroup"));
+		workerGroup = new NioEventLoopGroup(3, new DefaultThreadFactory("NettyWorkerSelectorGroup"));
 		defaultEventExecutorGroup = new DefaultEventExecutorGroup(8, new DefaultThreadFactory("NettyExecGroup"));
 
 		ServerBootstrap b = new ServerBootstrap();
 		b.group(bossGroup, workerGroup)
 				.channel(NioServerSocketChannel.class)
-				.option(ChannelOption.SO_BACKLOG, 1024).option(ChannelOption.TCP_NODELAY, true)
-				.option(ChannelOption.SO_TIMEOUT, 6000)
-				.childOption(ChannelOption.SO_REUSEADDR, true).childOption(ChannelOption.SO_KEEPALIVE, true)
+				.option(ChannelOption.SO_BACKLOG, 1024)
+				.option(ChannelOption.SO_REUSEADDR, true)
+				.option(ChannelOption.SO_KEEPALIVE, false)
+				.childOption(ChannelOption.TCP_NODELAY, true)
+				//.childOption(ChannelOption.SO_SNDBUF, nettyServerConfig.getServerSocketSndBufSize())
+				//.childOption(ChannelOption.SO_RCVBUF, nettyServerConfig.getServerSocketRcvBufSize())
 				.childOption(ChannelOption.ALLOCATOR, PooledByteBufAllocator.DEFAULT);
 
 		b.childHandler(new ChannelInitializer<SocketChannel>() {
@@ -90,6 +96,8 @@ public class NettyServer {
 						new LoggingHandler("com.ezweb.SERVLER", LogLevel.DEBUG),
 						new NettyDecoder(),
 						new NettyEncoder(),
+						new IdleStateHandler(0, 0, 10, TimeUnit.SECONDS),
+						new NettyConnectManageHandler(),
 						serverHandlerCreator.create()
 				);
 			}
@@ -104,4 +112,57 @@ public class NettyServer {
 			NettyServer.this.stop();
 		}
 	}
+
+	class NettyConnectManageHandler extends ChannelDuplexHandler {
+	        @Override
+	        public void channelRegistered(ChannelHandlerContext ctx) throws Exception {
+	            final String remoteAddress = RemotingUtil.parseChannelRemoteAddr(ctx.channel());
+		        logger.info("NETTY SERVER PIPELINE: channelRegistered {}", remoteAddress);
+	            super.channelRegistered(ctx);
+	        }
+
+	        @Override
+	        public void channelUnregistered(ChannelHandlerContext ctx) throws Exception {
+	            final String remoteAddress = RemotingUtil.parseChannelRemoteAddr(ctx.channel());
+		        logger.info("NETTY SERVER PIPELINE: channelUnregistered, the channel[{}]", remoteAddress);
+	            super.channelUnregistered(ctx);
+	        }
+
+	        @Override
+	        public void channelActive(ChannelHandlerContext ctx) throws Exception {
+	            final String remoteAddress = RemotingUtil.parseChannelRemoteAddr(ctx.channel());
+		        logger.info("NETTY SERVER PIPELINE: channelActive, the channel[{}]", remoteAddress);
+	            super.channelActive(ctx);
+	        }
+
+	        @Override
+	        public void channelInactive(ChannelHandlerContext ctx) throws Exception {
+	            final String remoteAddress = RemotingUtil.parseChannelRemoteAddr(ctx.channel());
+		        logger.info("NETTY SERVER PIPELINE: channelInactive, the channel[{}]", remoteAddress);
+	            super.channelInactive(ctx);
+	        }
+
+	        @Override
+	        public void userEventTriggered(ChannelHandlerContext ctx, Object evt) throws Exception {
+	            if (evt instanceof IdleStateEvent) {
+	                IdleStateEvent event = (IdleStateEvent) evt;
+	                if (event.state().equals(IdleState.ALL_IDLE)) {
+	                    final String remoteAddress = RemotingUtil.parseChannelRemoteAddr(ctx.channel());
+		                logger.warn("NETTY SERVER PIPELINE: IDLE exception [{}]", remoteAddress);
+	                    RemotingUtil.closeChannel(ctx.channel());
+	                }
+	            }
+
+	            ctx.fireUserEventTriggered(evt);
+	        }
+
+	        @Override
+	        public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+	            final String remoteAddress = RemotingUtil.parseChannelRemoteAddr(ctx.channel());
+		        logger.warn("NETTY SERVER PIPELINE: exceptionCaught {}", remoteAddress);
+		        logger.warn("NETTY SERVER PIPELINE: exceptionCaught exception.", cause);
+
+		        RemotingUtil.closeChannel(ctx.channel());
+	        }
+	    }
 }
