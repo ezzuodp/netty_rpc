620c691f5fd0438b94f7f40b4ac43bc4329ae217
dengpeng.zuo <cqdzuodp@163.com>
2017-12-26 16:28:42 +0800

    add http2.

---
+++ src/main/java/com/ezweb/engine/rpc/serialize/proto/ProtoEncoder.java
@@ -0,0 +1,21 @@
+package com.ezweb.engine.rpc.serialize.proto;
+
+import com.ezweb.engine.rpc.serialize.Encoder;
+
+import java.nio.ByteBuffer;
+
+/**
+ * <一句话说明功能>
+ * <功能详细描述>
+ *
+ * @author zuodengpeng
+ * @version 1.0.0
+ * @date 2017/12/8
+ */
+public class ProtoEncoder implements Encoder {
+
+	@Override
+	public void encode(Object object, ByteBuffer outbytes) throws Exception {
+
+	}
+}
