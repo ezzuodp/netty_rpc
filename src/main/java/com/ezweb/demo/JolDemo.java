37a8ec752f1b17d9f0b9f92d2f454d4c2f5a02bc
dengpeng.zuo <cqdzuodp@163.com>
2018-01-19 16:57:12 +0800

    提交打印对象

--- src/main/java/com/ezweb/demo/JolDemo.java
+++ src/main/java/com/ezweb/demo/JolDemo.java
@@ -17,8 +17,35 @@ public class JolDemo {
 	}
 
 	public static void main(String[] args) {
-		ClassData classData = ClassData.parseInstance(new Item());
-		CurrentLayouter currentLayouter = new CurrentLayouter();
-		System.out.println(currentLayouter.layout(classData).toPrintable());
+		Item o = new Item();
+		/*
+		com.ezweb.demo.JolDemo$Item object internals:
+		 OFFSET  SIZE   TYPE DESCRIPTION                               VALUE
+		      0     4        (object header)                           01 00 00 00 (00000001 00000000 00000000 00000000) (1)
+		      4     4        (object header)                           00 00 00 00 (00000000 00000000 00000000 00000000) (0)
+		      8     4        (object header)                           05 c1 00 f8 (00000101 11000001 00000000 11111000) (-134168315)
+		     12     4        (loss due to the next object alignment)
+		Instance size: 16 bytes
+		Space losses: 0 bytes internal + 4 bytes external = 4 bytes total
+
+		com.ezweb.demo.JolDemo$Item object internals:
+		 OFFSET  SIZE   TYPE DESCRIPTION                               VALUE
+		      0     4        (object header)                           88 f2 f8 02 (10001000 11110010 11111000 00000010) (49869448)
+		      4     4        (object header)                           00 00 00 00 (00000000 00000000 00000000 00000000) (0)
+		      8     4        (object header)                           05 c1 00 f8 (00000101 11000001 00000000 11111000) (-134168315)
+		     12     4        (loss due to the next object alignment)
+		Instance size: 16 bytes
+		Space losses: 0 bytes internal + 4 bytes external = 4 bytes total
+		 */
+		synchronized (o) {
+			ClassData classData = ClassData.parseInstance(o);
+			CurrentLayouter currentLayouter = new CurrentLayouter();
+			System.out.println(currentLayouter.layout(classData).toPrintable());
+		}
+		{
+			ClassData classData = ClassData.parseInstance(o);
+			CurrentLayouter currentLayouter = new CurrentLayouter();
+			System.out.println(currentLayouter.layout(classData).toPrintable());
+		}
 	}
 }
