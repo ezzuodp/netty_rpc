0ca6bc1ad3c82557a56e6a6891b349fcbfcc15bd
dengpeng.zuo <cqzuodp@163.com>
2018-04-07 21:54:19 +0800

    add:use rbtree 保存数据.

--- src/test/java/com/ezweb/netty/NettyByteBufTest.java
+++ src/test/java/com/ezweb/netty/NettyByteBufTest.java
@@ -5,6 +5,9 @@ import com.ezweb.engine.util.PooledAllocatorStats;
 import com.google.common.collect.Maps;
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.PooledByteBufAllocator;
+import io.netty.util.concurrent.DefaultThreadFactory;
+import io.netty.util.concurrent.FastThreadLocal;
+import io.netty.util.concurrent.FastThreadLocalThread;
 import org.springframework.jmx.export.MBeanExporter;
 
 import java.util.Map;
@@ -24,9 +27,11 @@ public class NettyByteBufTest {
 		// 关闭回收机制.
 		System.setProperty("io.netty.recycler.maxCapacityPerThread", "0");
 		// 关闭线程局部缓存[]
+		/*
 		System.setProperty("io.netty.allocator.tinyCacheSize", "0");
 		System.setProperty("io.netty.allocator.smallCacheSize", "0");
 		System.setProperty("io.netty.allocator.normalCacheSize", "0");
+		*/
 	}
 
 	public static void main(String[] args) throws InterruptedException {
@@ -42,11 +47,24 @@ public class NettyByteBufTest {
 
 		PooledByteBufAllocator alloc = PooledByteBufAllocator.DEFAULT;
 
-		for (int j = 0; j < 100000; ++j) {
-			ByteBuf buf = alloc.directBuffer(8192);
-			buf.release();
-		}
+		// 自己写的线程，必须手动调用[FastThreadLocal.removeAll();]才能放掉线程中的缓存分配字节数.
+		// 如果是用 FastThreadLocalThread 就不用自己手动 FastThreadLocal.removeAll();
+		FastThreadLocalThread t = new FastThreadLocalThread(new Runnable() {
+			@Override
+			public void run() {
+				for (int j = 0; j < 100000; ++j) {
+					ByteBuf buf = alloc.directBuffer(8192);
+					buf.release();
+				}
+				System.out.println("alloc 100 * 1000 =>>>> ");
+			}
+		}, "TestAllocThread");
 
-		TimeUnit.SECONDS.sleep(60L);
+		t.setDaemon(true);
+		t.start();
+
+		// 关闭 局部线程池::: allocationsNormal = 100 * 1000, deallocationsNormal = 100 * 1000
+		TimeUnit.SECONDS.sleep(10L);
+		System.out.println("t = " + t);
 	}
 }
