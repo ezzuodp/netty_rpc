cba7156bcf393066cac78f62955e6b1c21aca5fd
zuodengpeng <cqzuodp@163.com>
2018-05-26 10:15:43 +0800

    add:解决多线程并发.

--- src/main/java/com/ezweb/interview/ArraySort.java
+++ src/main/java/com/ezweb/interview/ArraySort.java
@@ -1,10 +1,12 @@
 package com.ezweb.interview;
 
+import com.ezweb.engine.util.MaxHeap;
+
 import java.security.SecureRandom;
 import java.util.Arrays;
 
 /**
- * 数组排序算法
+ * 数组排序算法(插入|堆|归并)
  *
  * @author : zuodp
  * @version : 1.10
@@ -27,6 +29,28 @@ public class ArraySort {
 		if (count >= 1) {
 			count = (len - count) / 2 + count - 1;
 			System.out.println(String.format("Find val:%d cur:%d", result, count));
+		}
+	}
+
+	/**
+	 * 将数组中[基数在前，偶数在后]
+	 */
+	static void move2End(int[] array, int len) {
+		int sj = len - 1;
+		for (int i = 0; i < sj; ++i) {
+			if (array[i] % 2 == 0) {
+				int t = array[i];
+				for (int j = sj; j > i; --j) {
+					if (array[j] % 2 == 0) {
+
+					} else {
+						array[i] = array[j];
+						array[j] = t;
+						sj = j - 1;
+						break;
+					}
+				}
+			}
 		}
 	}
 
@@ -61,60 +85,104 @@ public class ArraySort {
 	/**
 	 * insert sort
 	 */
-	static void sort(int[] array, int len) {
-		int right = len - 1;             // right 必须是len-1
-		for (int i = 0, j = i; i < right; j = ++i) {
-			int tmp = array[i + 1];       // 直接取下一个数组值.
-
-			while (array[j] > tmp) {
-				array[j + 1] = array[j]; // array[j]后移到a[j+1]
-				--j;
-				if (j < 0) {             // **注意必须是到-1.
+	static void insertSort(int[] array, int len) {
+		for (int j = 1; j < len; ++j) {
+			int tmp = array[j];
+			int i = 0;
+			for (i = j; i > 0; --i) {
+				if (array[i - 1] > tmp) {       // 写错了，一定要array[i-1]
+					array[i] = array[i - 1];    // 写错了，一定要array[i]=array[i-1].
+				} else {
 					break;
 				}
 			}
-			array[j + 1] = tmp;
+			array[i] = tmp;
 		}
 	}
 
 	/**
-	 * 将数组中[基数在前，偶数在后]
+	 * heap sort
 	 */
-	static void move2End(int[] array, int len) {
-		int sj = len - 1;
-		for (int i = 0; i < sj; ++i) {
-			if (array[i] % 2 == 0) {
-				int t = array[i];
-				for (int j = sj; j > i; --j) {
-					if (array[j] % 2 == 0) {
-
-					} else {
-						array[i] = array[j];
-						array[j] = t;
-						sj = j - 1;
-						break;
-					}
-				}
-			}
+	static void heapSort(int[] array, int len) {
+		MaxHeap<Integer> heap = new MaxHeap<Integer>(len) {
+		};
+		for (int i = 0; i < len; ++i) {
+			heap.push(array[i]);
+		}
+		for (int i = len - 1; i >= 0; --i) {
+			int v = heap.pop();
+			array[i] = v;
 		}
 	}
 
-	static void random(int[] array, @SuppressWarnings("unused") int len) {
+	/**
+	 * 归并排序
+	 */
+	static void mergeSort(int[] elements, int len) {
+		int[] aux = new int[len];
+		int start = 0, end = len - 1;
+		doMergeSort(elements, start, end, aux);
+	}
+
+	static void doMergeSort(int[] elements, int start, int end, int[] aux) {
+		if (start >= end) {
+			return;
+		}
+		int mid = (start + end) / 2;
+		doMergeSort(elements, start, mid, aux);
+		doMergeSort(elements, mid + 1, end, aux);
+		merge(elements, start, mid, end, aux);
+	}
+
+	static void merge(int[] elements, int start, int mid, int end, int[] aux) {
+		int lb = start, rb = mid + 1, auxIndex = start;
+		while (lb <= mid && rb <= end) {
+			if (Integer.compare(elements[lb], elements[rb]) <= 0) {
+				aux[auxIndex++] = elements[lb++];
+			} else {
+				aux[auxIndex++] = elements[rb++];
+			}
+		}
+
+		if (lb < mid + 1) {
+			while (lb <= mid) {
+				aux[auxIndex++] = elements[lb++];
+			}
+		} else {
+			while (rb <= end) {
+				aux[auxIndex++] = elements[rb++];
+			}
+		}
+
+		for (int i = start; i <= end; i++) {
+			elements[i] = aux[i];
+		}
+	}
+
+	static void randomBytes(int[] array, @SuppressWarnings("unused") int len) {
 		SecureRandom sr = new SecureRandom();
 		Arrays.setAll(array, (int operand) -> Math.abs(sr.nextInt(1000)));
 	}
 
 	public static void main(String[] args) {
 		int[] array = new int[128];
-		random(array, 128);
+
+		randomBytes(array, 128);
+		insertSort(array, 128);
 		System.out.println(Arrays.toString(array));
-		sort(array, 128);
-		System.out.println(Arrays.toString(array));
-		find(array, 128, array[127]);
-		move2End(array, 128);
-		sort(array, 128);
+		find(array, 128, array[123]);
 		find(array, 128, array[127]);
 
-		wxcount(new int[]{1, 4, 4, 4, 6, 4, 4, 9, 9}, 9);
+		randomBytes(array, 128);
+		heapSort(array, 128);
+		System.out.println(Arrays.toString(array));
+		find(array, 128, array[123]);
+		find(array, 128, array[127]);
+
+		randomBytes(array, 128);
+		mergeSort(array, 128);
+		System.out.println(Arrays.toString(array));
+		find(array, 128, array[123]);
+		find(array, 128, array[127]);
 	}
 }
