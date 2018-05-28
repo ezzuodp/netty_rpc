2ed9326e99e7e9eb5865b7d03da06b97f096349b
dengpeng.zuo <cqzuodp@163.com>
2018-05-28 09:35:42 +0800

    fix:协议加上gzip.

--- src/main/java/com/ezweb/engine/rpc/server/RpcServerHandler.java
+++ src/main/java/com/ezweb/engine/rpc/server/RpcServerHandler.java
@@ -5,12 +5,16 @@ import com.ezweb.engine.CustTType;
 import com.ezweb.engine.rpc.RpcProtocolProcessor;
 import com.ezweb.engine.server.AbsServerHandler;
 import io.netty.channel.ChannelHandlerContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * @author : zuodp
  * @version : 1.10
  */
 public class RpcServerHandler extends AbsServerHandler {
+	private static final Logger LOGGER = LoggerFactory.getLogger(RpcServerHandler.class);
+
 	private RpcProtocolProcessor protocolProcessor = null;
 
 	public RpcServerHandler(RpcProtocolProcessor protocolProcessor) {
@@ -18,7 +22,12 @@ public class RpcServerHandler extends AbsServerHandler {
 	}
 
 	@Override
-	public CustTMessage handleCustTMessage(ChannelHandlerContext ctx, CustTMessage request) throws Exception {
+	protected void handleHeartBeatCustTMessage(ChannelHandlerContext ctx, CustTMessage request) {
+		LOGGER.debug("receive channel: <{}> heartbeat req:{}.", ctx.channel(), request);
+	}
+
+	@Override
+	protected CustTMessage handleCustTMessage(ChannelHandlerContext ctx, CustTMessage request) throws Exception {
 		CustTMessage response = null;
 		if (request.getType() == CustTType.CALL) {
 			// 这儿就可以另起 bizThread 进行 rpc 业务调用...
