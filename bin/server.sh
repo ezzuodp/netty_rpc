5c8432372d93e9bc32f1a832fd6f500f17291095
dengpeng.zuo <cqzuodp@163.com>
2018-11-29 19:58:47 +0800

    fix:重构一下.

--- bin/server.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-cur_dir=`pwd`
-sh ${cur_dir}/tool.sh com.ezweb.demo.ServerBootstrap $@
--- src/main/java/com/ezweb/engine/server/ServerHandlerCreator.java
+++
@@ -1,9 +0,0 @@
-package com.ezweb.engine.server;
-
-/**
- * @author : zuodp
- * @version : 1.10
- */
-public interface ServerHandlerCreator<H extends AbsServerHandler> {
-	H create() throws Exception;
-}
